9e88794c0f5e17cf63e1213f93ed60ebe05c709b
Daniel <dluiscamargo@gmail.com>
2025-01-28 01:11:48 +0000

    models migratio

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support)//injeção de dependencias do laravel
     {
-        return view('admin/supports/index');
+        // $support = new Support();
+        $supports = $support->all();
+        dd($supports);
+
+        return view('admin/supports/index', compact('supports'));
 
     }
 }
