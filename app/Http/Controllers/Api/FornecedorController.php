82364a53983d45aec28a8d426cc37a2e57dab8b9
Daniel <dluiscamargo@gmail.com>
2025-02-06 02:06:48 +0000

    list records with pagination api REST

--- app/Http/Controllers/Api/FornecedorController.php
+++ app/Http/Controllers/Api/FornecedorController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\DTO\CreateFornecedorDTO;
+use App\DTO\UpdateFornecedorDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateFornecedor;
 use App\Http\Resources\FornecedorResource;
+use App\Models\Fornecedor;
 use Illuminate\Http\Request;
 use App\Services\FornecedorService;
 
@@ -20,9 +22,31 @@ class FornecedorController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        // paginator default ok
+        // $fornecedores = Fornecedor::paginate();
+        // return FornecedorResource::collection($fornecedores);
+
+
+        $fornecedores = $this->service->paginate(
+            page: $request->get('page', 1),
+            totalPerPage: $request->get('per_page', 3),
+            filter: $request->filter,
+        );
+
+
+        return FornecedorResource::collection($fornecedores->item())
+                                   ->additional([
+                                    'meta' => [
+                                        'total' => $fornecedores->total(),
+                                        'is_first_page' => $fornecedores->isFirstPage(),
+                                        'is_last_page' => $fornecedores->isLastPage(),
+                                        'current_page' => $fornecedores->currentPage(),
+                                        'next_page' => $fornecedores->getNumberNextPage(),
+                                        'previous_page' => $fornecedores->getNumberPreviousPage(),
+                                    ]
+                                ]);
     }
 
     /**
@@ -42,15 +66,31 @@ class FornecedorController extends Controller
      */
     public function show(string $id)
     {
-        //
+        if (!$fornecedor = $this->service->findOne($id)){
+            return response()->json([
+                'error' => 'Not Found'
+            ], 404);
+        }
+
+        return new FornecedorResource($fornecedor);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StoreUpdateFornecedor $request, string $id)
     {
-        //
+        $fornecedor = $this->service->update(
+            UpdateFornecedorDTO::makeFromRequest($request)
+        );
+
+        if (!$fornecedor) {
+            return response()->json([
+                'error' => 'Not Found'
+            ], 404);
+        }
+
+        return new FornecedorResource($fornecedor);
     }
 
     /**
@@ -58,6 +98,16 @@ class FornecedorController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        if (!$this->service->findOne($id)){
+            return response()->json([
+                'error' => 'Not Found'
+            ], 404);
+        }
+
+        $this->service->delete($id);
+
+        return response()->json([
+            'No Content' => 'No Content'
+        ], 204);
     }
 }
